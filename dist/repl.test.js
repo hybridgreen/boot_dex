ea31f8d962b72d06a904cbcacbce23545867279e
hybridgreen <y.yayaoye@gmail.com>
2025-08-14 16:30:24 +0100

    Initial commit: Working the REPL

---
+++ dist/repl.test.js
@@ -0,0 +1,25 @@
+import { cleanInput } from "./repl";
+import { describe, expect, test } from "vitest";
+describe.each([
+    {
+        input: "    Hello world      ",
+        expected: ["hello", "world"]
+    },
+    {
+        input: "    Charmander Bulbasaur PIKACHU   ",
+        expected: ["charmander", "bulbasaur", "pikachu"]
+    },
+    {
+        input: "    RAIKOU Suicune Entei ",
+        expected: ["raikou", "suicune", "entei"]
+    }
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            // likewise, the `toBe` function will fail the test if the values are not equal
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
---
+++ src/repl.ts
@@ -0,0 +1,26 @@
+import {createInterface} from "node:readline";
+
+export function cleanInput(input: string): string[]{
+    return input.trim().toLowerCase().split(" ");
+}
+
+export function startREPL(){
+    const read = createInterface(
+        {
+            input: process.stdin,
+            output: process.stdout,
+            prompt:"Pokedex > "
+        });
+    
+    read.prompt();
+    const stream = read.on("line", (input) => {
+        const words = cleanInput(input);
+        if (words.length === 0){
+            read.prompt()
+        }
+        else{
+            console.log(`Your command was: ${words[0]}`);
+            read.prompt();
+        }
+    });
+}
